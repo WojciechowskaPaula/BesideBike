b2db11a1890f40ac9924b8bf8f8353a53f21a3c4
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-02-17 15:57:00 +0100

    Add BikeRepository Interface

---
+++ BesideBike.Domain/Interface/IBikeRepository.cs
@@ -0,0 +1,14 @@
+﻿using BesideBike.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BesideBike.Domain.Interface
+{
+    public interface IBikeRepository
+    {
+        IEnumerable<Bike> GetAllBikes();
+    }
+}
